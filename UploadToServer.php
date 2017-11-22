0f65f27b2e14cf09336537196a85e6089820d0b4
Chaynika Saikia <csaikia@asu.edu>
2017-11-22 01:05:10 -0700

    Update UploadToServer.php

--- UploadToServer.php
+++ UploadToServer.php
@@ -1,8 +1,6 @@
 <?php
     $file_path = "/Users/csaikia/Dropbox/Coursework/CSE535/uploaded_files/"; 
     $file_path = $file_path . basename( $_FILES['files']['name']);
-    echo $file_path;
-    echo $_FILES['files']['tmp_name'];
     if(move_uploaded_file($_FILES['files']['tmp_name'], $file_path)) {
         echo "success";
     } else{
